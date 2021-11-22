840bd495ee467a0085ddf983c7e1e83a5fd19735
maekawatatsuya1217 <84844702+maekawatatsuya1217@users.noreply.github.com>
2021-11-22 10:35:33 +0900

    変数と文字列を連結

--- index.php
+++ index.php
@@ -1,28 +1,4 @@
 <?php
-
-    $x = 5;
-    $y = 2;
-    $a = 8;
-    $b = 4;
-    
-?>
-
-<?php
-    $x += 10;
-    echo $x;
-?>
-
-<?php
-    $y *= 5;
-    echo $y;
-?>
-
-<?php
-    $a ++;
-    echo $a;
-?>
-  
-<?php
-    $b --;
-    echo $b;
+    $name = '前川さん!';
+    echo "こんにちは！".$name;
 ?>

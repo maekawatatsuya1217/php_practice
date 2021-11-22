9185545b0d93e2cf7af283cbdaea6c1c51a7d52e
maekawatatsuya1217 <84844702+maekawatatsuya1217@users.noreply.github.com>
2021-11-22 10:30:20 +0900

    変数を利用して計算

--- index.php
+++ index.php
@@ -1,9 +1,28 @@
 <?php
-  $fruit = "パイナップル";
-  echo $fruit
+
+    $x = 5;
+    $y = 2;
+    $a = 8;
+    $b = 4;
+    
 ?>
 
 <?php
-  $sum = 5 + 5;
-  echo $sum;
+    $x += 10;
+    echo $x;
+?>
+
+<?php
+    $y *= 5;
+    echo $y;
+?>
+
+<?php
+    $a ++;
+    echo $a;
+?>
+  
+<?php
+    $b --;
+    echo $b;
 ?>

c05ee007ec0a6fd577435d2b2c843d3344c76bc5
maekawatatsuya1217 <84844702+maekawatatsuya1217@users.noreply.github.com>
2021-11-23 19:00:14 +0900

    switch文を利用

--- index.php
+++ index.php
@@ -1,15 +1,25 @@
 <?php
+
+    $num = 0;
   
-    $x = 1071;
-    
-    if ($x % 3 == 0 && $x % 7 == 0) {
-      echo "xは3の倍数かつ7の倍数です。";
-    } elseif ($x % 3 == 0) {
-      echo "xは3の倍数ですが7の倍数ではありません。";
-    } elseif ($x % 7 == 0) {
-      echo "xは7の倍数ですが3の倍数ではありません。";
-    } else {
-      echo "xは7の倍数でも3の倍数でもありません。";
-    }
-    
+    $remainder = $num % 3;
+ 
+    switch ($remainder) {
+      case 0:
+        echo "大吉です。";
+        break;
+        
+      case 1:
+        echo "中吉です。";
+        break;
+        
+      case 2:
+        echo "小吉です。";
+        break;
+        
+      default:
+        echo "凶です。";
+        break;
+    };
+  
 ?>

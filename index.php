fa4ef7ee1ea963200caf819c260c9095a0403db7
maekawatatsuya1217 <84844702+maekawatatsuya1217@users.noreply.github.com>
2021-11-23 19:28:21 +0900

    繰り返し処理を学習

--- index.php
+++ index.php
@@ -1,13 +1,8 @@
 <?php
 
-    $scores = array(
-        "数学" => 70,
-        "英語" => 90,
-        "国語" => 80,
-      );
-      
-    $scores["国語"] = $scores["国語"]+= 5;
-    
-    echo $scores["国語"];
+    for ($sum = 51; $sum <= 100; $sum++) {
+      echo $sum;
+      echo "<br>";
+    };
     
 ?>

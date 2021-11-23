af5a391a7be5a442c39f7df43593309972d0e6ac
maekawatatsuya1217 <84844702+maekawatatsuya1217@users.noreply.github.com>
2021-11-23 19:19:18 +0900

    理想配列を学習

--- index.php
+++ index.php
@@ -1,11 +1,13 @@
 <?php
 
-    $colors = array ('赤', '青', '黄');
+    $scores = array(
+        "数学" => 70,
+        "英語" => 90,
+        "国語" => 80,
+      );
+      
+    $scores["国語"] = $scores["国語"]+= 5;
     
-    echo $colors[0];
-    
-    $colors[] = "白";
-    
-    echo $colors[3];
+    echo $scores["国語"];
     
 ?>

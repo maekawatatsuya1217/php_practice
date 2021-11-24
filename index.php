4a8d1677c5cfa0bb0063f329ab9b9d8ceeb1d583
maekawatatsuya1217 <84844702+maekawatatsuya1217@users.noreply.github.com>
2021-11-24 21:13:26 +0900

    continue文を学習

--- index.php
+++ index.php
@@ -1,11 +1,11 @@
 <?php
 
-for ($i = 1; $i <= 1000; $i ++) {
-  if ($i >= 501) {
-    break;
-  }
-  echo $i;
-  echo '<br>';
-}
-
+    for ($i = 1; $i <= 1000; $i++) {
+      if ($i % 3 == 0) {
+        continue;
+      }
+      echo $i;
+      echo '<br>';
+    }
+    
 ?>

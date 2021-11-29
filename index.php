37bab986fa76767f11ae901612ebc928d5da1ae9
maekawatatsuya1217 <84844702+maekawatatsuya1217@users.noreply.github.com>
2021-11-29 15:30:58 +0900

    計算について復習

--- index.php
+++ index.php
@@ -1,9 +1,13 @@
 <?php
-$name = 'Tom';
-echo '変数$nameの値: '.$name;
+$price = 1000;
+$taxRate = 0.08;
+echo '変数$priceの値: '.$price;
+echo '<br>';
+echo '変数$taxRateの値: '.$taxRate;
 echo '<br>';
 echo '-----';
 echo '<br>';
 
-echo "My name is {$name}";
+$num = $price + $price * $taxRate;
+echo "税込価格は{$num}円です";
 ?>

7ba0801aa0036f43922c94f83efc778d2cfad430
maekawatatsuya1217 <84844702+maekawatatsuya1217@users.noreply.github.com>
2021-11-29 10:27:24 +0900

    フォームの送信と受信を学習

--- index.php
+++ index.php
@@ -1,11 +1,16 @@
-<?php
+<div class="display-contact">
 
-    function getCircleArea($radius) {
-      return $radius * $radius * 3;
-    }
-    
-    $circleArea = getCircleArea(5);
-    
-    echo $circleArea;
-    
-?>
+    <div class="form-title">入力内容</div>
+
+    <div class="form-item">■ 名前</div>
+
+    <?php
+      echo $_POST["name"];
+    ?>
+    <div class="form-item">■ 内容</div>
+
+    <?php
+      echo $_POST["body"];
+    ?>
+
+</div>
